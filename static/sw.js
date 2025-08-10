4101acf259aa30f174f8beaeed452633ad337b5a
Abdullah Tariq <159679357+Abdullah9779@users.noreply.github.com>
2025-08-10 15:58:04 +0500

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'dotcoder-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icons/dotcoder-logo-192.png',
+  '/static/icons/dotcoder-logo-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker: Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Service Worker: Caching files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker: Activating...');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+}); 
